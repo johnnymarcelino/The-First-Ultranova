074aec725b186418ac4944c66111efb0a8660de6
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2023-02-15 22:56:42 -0300

    feature: arrays, bool, objects

---
+++ Fundamentos/objeto.js
@@ -0,0 +1,14 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 5344.543
+prod1['Desconto Legal'] = 0.40
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 589.32
+}
+
+console.log(prod2)
+
