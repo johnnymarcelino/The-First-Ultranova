9da0149f07e9e076ed74c680bde2338b7d4c7713
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2023-03-17 23:58:10 -0300

    static and dinamic content

---
+++ javascript/funcao.js/estaticDinamic.js
@@ -0,0 +1,14 @@
+// Conteúdo estático
+let nome = "João";
+let sobrenome = "Silva";
+let saudacao = "Olá, " + nome + " " + sobrenome + "! Bem-vindo à nossa página.";
+
+console.log(saudacao); // Imprime: Olá, João Silva! Bem-vindo à nossa página.
+
+
+// Conteúdo dinâmico
+nome = "Maria";
+sobrenome = "Souza";
+saudacao = "Olá, " + nome + " " + sobrenome + "! Bem-vindo à nossa página.";
+
+console.log(saudacao); // Imprime: Olá, Maria Souza! Bem-vindo à nossa página.
--- javascript/funcao.js/this.js
+++ javascript/funcao.js/this.js
@@ -3,29 +3,29 @@
 // Por exemplo, quando usado dentro de uma função, "this" se refere ao objeto global.
 
 function myFunction() {
-    console.log(this); // Logs o objeto global
+  console.log(this); // Logs o objeto global
+}
+
+// Quando usado dentro de um objeto, "this" se refere ao próprio objeto.
+
+let myObject = {
+  name: "John",
+  sayName: function () {
+    console.log(this.name); // Logs "John"
   }
-  
-  // Quando usado dentro de um objeto, "this" se refere ao próprio objeto.
-  
-  let myObject = {
-    name: "John",
-    sayName: function() {
-      console.log(this.name); // Logs "John"
-    }
-  };
-  
-  // Quando usado dentro de uma classe, "this" se refere à instância da classe.
-  
-  class MyClass {
-    constructor(name) {
-      this.name = name;
-    }
-    
-    sayName() {
-      console.log(this.name); // Logs o nome da instância
-    }
+};
+
+// Quando usado dentro de uma classe, "this" se refere à instância da classe.
+
+class MyClass {
+  constructor(name) {
+    this.name = name;
   }
-  
-  let myInstance = new MyClass("John");
-  myInstance.sayName(); // Logs "John"
+
+  sayName() {
+    console.log(this.name); // Logs o nome da instância
+  }
+}
+
+let myInstance = new MyClass("John");
+myInstance.sayName(); // Logs "John"
