f8c6e5cee60f36df30e6e0222716154ae1694cde
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2023-03-18 23:42:38 -0300

    arrow function - part 1

---
+++ javascript/funcao.js/arrow.js
@@ -0,0 +1,16 @@
+// Definição de uma função arrow
+const soma = (a, b) => {
+    return a + b;
+  }
+
+// Chamada da função arrow
+console.log(soma(2, 3)); // Imprime: 5
+
+
+///////////////////////////////////////////////
+
+// Definição de uma função arrow com sintaxe reduzida
+const quadrado = x => x * x;
+
+// Chamada da função arrow
+console.log(quadrado(5)); // Imprime: 25
