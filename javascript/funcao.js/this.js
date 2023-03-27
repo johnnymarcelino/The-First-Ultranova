dd0a49c2b691985609e89c6ce485b407e545aa71
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2023-03-26 23:16:58 -0300

    feat: using this word with bind function

--- javascript/funcao.js/this.js
+++ javascript/funcao.js/this.js
@@ -29,3 +29,69 @@ class MyClass {
 
 let myInstance = new MyClass("John");
 myInstance.sayName(); // Logs "John"
+
+/*
+
+// usando a função bind -> referenciar a palavra 'this'
+
+// Exemplo 1 - usando bind para passar argumentos para uma função
+function adicionar(a, b) {
+  return a + b;
+}
+
+let adicionarDoisNumeros = adicionar.bind(this, 2);
+console.log(adicionarDoisNumeros(4)); // resultado 6
+
+// Exemplo 2 - usando bind para definir o contexto de execução de uma função
+let obj = {
+  nome: 'John'
+};
+
+function saudar() {
+  return 'Olá, ' + this.nome;
+}
+
+let saudarJohn = saudar.bind(obj);
+console.log(saudarJohn()); // resultado 'Olá, John'
+
+// Exemplo 3 - usando bind para combinar duas funções
+function saudar(greeting, punctuation) {
+  return greeting + ' ' + this.nome + punctuation;
+}
+
+let saudarJohnComPontoFinal = saudar.bind('Olá', '.');
+console.log(saudarJohnComPontoFinal()); // resultado 'Olá John.'
+
+// O método bind() cria uma nova função que, quando chamada, tem seu contexto (this) setado para o objeto passado como primeiro parâmetro, com os argumentos passados depois (se houver).
+
+Exemplo de uso:
+
+function adicionar(a, b) {
+    return a + b;
+}
+
+// usando bind para passar argumentos para uma função
+let adicionarDoisNumeros = adicionar.bind(this, 2);
+console.log(adicionarDoisNumeros(4)); // resultado 6
+
+// usando bind para definir o contexto de execução de uma função
+let obj = {
+    nome: 'John'
+};
+
+function saudar() {
+    return 'Olá, ' + this.nome;
+}
+
+let saudarJohn = saudar.bind(obj);
+console.log(saudarJohn()); // resultado 'Olá, John'
+
+// usando bind paracontinueombinar duas funções
+function saudar(greeting, punctuation) {
+    return greeting + ' ' + this.nome + punctuation;
+}
+
+let saudarJohnComPontoFinal = saudar.bind(continue, 'Olá', '.');
+console.log(saudarJohnComPontoFinal()); // resultado 'Olá John.'
+
+*/
