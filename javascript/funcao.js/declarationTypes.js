6e57c7f1ac758b0788e8f3e23ddaa313519689bb
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2023-04-03 17:43:17 -0300

    feat: types of functions declation

---
+++ javascript/funcao.js/declarationTypes.js
@@ -0,0 +1,21 @@
+// In JavaScript, there are several ways to declare functions. Here are some simple and concrete examples of different types of function declarations:
+
+// Function Declaration: A function declaration is a named function that is defined using the function keyword.
+
+function sayHello(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+// Function Expression: A function expression is an anonymous function that is assigned to a variable.
+
+const sayHello = function(name) {
+  console.log(`Hello, ${name}!`);
+};
+
+// Arrow Function: An arrow function is a concise way to write a function expression using the => syntax
+
+const sayHello = (name) => {
+  console.log(`Hello, ${name}!`);
+};
+
+// These are just a few examples of how you can declare functions in JavaScript. Each type of function declaration has its own advantages and use cases.
