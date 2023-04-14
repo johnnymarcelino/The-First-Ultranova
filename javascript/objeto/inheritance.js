843cf453f8578bdefc53c3bb75d2b218dfe5f008
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2023-04-14 17:49:48 -0300

    feat: working with inheritance - part 2

--- javascript/objeto/inheritance.js
+++ javascript/objeto/inheritance.js
@@ -16,3 +16,41 @@ console.log(ferrari.prototype.__proto__ === null)
 function MeuObjeto() {}
 console.log(typeof Object, typeof MeuObjeto)
 console.log(Object.prototype, MeuObjeto.prototype)
+
+
+
+// Cadeia de protótipos (Prototype chain)
+
+Object.prototype.attr0 = '0'
+const avo = { attr1: 'A' }
+const pai = { __proto__: avo, attr2: 'B', attr3: '3' }
+const filho = { __proto__: pai, attr3: 'C' }
+console.log(filho.attr0, filho.attr1, filho.attr2, filho.attr3)
+
+const carro = {
+    velAtual: 0,
+    velMax: 200,
+    acelerarMais(delta) {
+        if (this.velAtual + delta <= this.velMax) {
+            this.velAtual += delta
+        }else {
+            this.velAtual = this.velMax
+        }
+    },
+    status() {
+        return `${this.modelo}: ${super.status()}`
+        // return `Velocidade atual: ${this.velAtual}km/h`
+    }
+}
+
+Object.setPrototypeOf(ferrario, carro)
+Object.setPrototypeOf(volvo, carro)
+
+console.log(ferrari)
+console.log(volvo)
+
+volvo.acelerarMais(300)
+console.log(volvo.status())
+
+ferrari.acelerarMais(300)
+console.log(ferrari.status())
