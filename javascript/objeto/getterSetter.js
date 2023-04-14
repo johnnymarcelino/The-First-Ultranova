5a6c661a34afb723bfbb29254d67baf01e9a9d4c
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2023-04-14 16:49:12 -0300

    feat: importants functions implemented

--- javascript/objeto/getterSetter.js
+++ javascript/objeto/getterSetter.js
@@ -13,3 +13,48 @@ sequencia.valor = 1000;
 console.log(sequencia.valor, sequencia.valor);
 sequencia.valor = 900;
 console.log(sequencia.valor, sequencia.valor);
+
+
+
+
+
+
+// OUTROS ASSUNTOS IMPORTANTES
+
+const pessoa = {
+    nome: 'Rebeca',
+    idade: 2,
+    peso: 13
+}
+
+console.log(Object.keys(pessoa));
+console.log(Object.values(pessoa));
+console.log(Object.entries(pessoa));
+
+Object.entries(pessoa).forEach(e => {
+    console.log(`${e[0]}: ${e[3]}`)
+})
+
+Object.entries(pessoa).forEach(([chave, valor]) => {
+    console.log(`${chave}: ${valor}`)
+})
+
+Object.defineProperties(pessoa, 'dataNascimento', {
+    enumerable: true,
+    writeble: false,
+    value: '01/01/2019'
+})
+
+pessoa.dataNascimento = '01/01/2017'
+console.log(pessoa.dataNascimento);
+console.log(Object.keys(pessoa));
+
+// Object.assign (ECMAScript 2015)
+const dest = { a: 1 }
+const o1 = { a: 1 }
+const o2 = { a: 1 }
+const obj = Object.assign(dest, 01, 02)
+
+// Object.freeze(obj)
+obj.c = 1234
+console.log(obj)
